5c1c660ad9184404329d95628cbfed66c2b5fedf
Colin Mattson <colinm@pixelcloud.us>
2013-07-18 16:10:17 -0700

    Implement Mongoid/AR adapter

--- lib/consort/mongoid.rb
+++ lib/consort/mongoid.rb
@@ -4,15 +4,27 @@ module Consort
 
     module ClassMethods
       def has_one_active_record(klass)
-
+        class_eval <<-CODE
+          def #{klass}
+            #{klass.to_s.classify}.where(#{name.foreign_key}: id)
+          end
+        CODE
       end
 
       def has_many_active_records(klass)
-
+        class_eval <<-CODE
+          def #{klass}
+            #{klass.to_s.classify}.where(#{name.foreign_key}: id)  
+          end
+        CODE
       end
 
       def belongs_to_active_record(klass)
-
+        class_eval <<-CODE
+          def #{klass}
+            #{klass.to_s.classify}.where(id: #{klass.to_s.foreign_key})
+          end
+        CODE
       end
 
       def mongoid_consorts_with_active_record?
