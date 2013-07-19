e542594a43497f120ec554c0f1099e038d1dd689
Colin Mattson <colinm@pixelcloud.us>
2013-07-19 08:14:57 -0700

    Converted to Ruby 1.9 hash syntax

--- test/db/schema.rb
+++ test/db/schema.rb
@@ -1,22 +1,22 @@
 ActiveRecord::Schema.define(:version => 0) do
 
-  create_table :ungulates, :force => true do |t|
+  create_table :ungulates, force: true do |t|
     t.column :name, :string
     t.column :congo_id, :string
   end
 
-  create_table :tongues, :force => true do |t|
+  create_table :tongues, force: true do |t|
     t.column :camel_id, :string
     t.column :slimy, :boolean
   end
 
-  create_table :humps, :force => true do |t|
+  create_table :humps, force: true do |t|
     t.column :camel_id, :string
     t.column :number, :integer
     t.column :size_mm, :integer
   end
 
-  create_table :species, :force => true do |t|
+  create_table :species, force: true do |t|
     t.column :common_name, :string
     t.column :scientific_name, :string
   end
