58f42b09e78ca61ae98d6abfb3ae8ce23e4ed058
Colin Mattson <colinm@pixelcloud.us>
2013-07-29 09:52:13 -0700

    Only load bridges for available ORM/ODM layers.

--- lib/consort.rb
+++ lib/consort.rb
@@ -3,5 +3,14 @@ require 'consort/version'
 #require 'active_record'
 #require 'mongoid'
 
-require 'consort/active_record/mongoid'
-require 'consort/mongoid/active_record'
+if defined?(ActiveRecord::Base) && defined?(Mongoid::Document)
+  require 'consort/active_record/mongoid'
+  require 'consort/mongoid/active_record'
+end
+
+if defined?(ActiveRecord::Base) && defined?(MongoMapper::Document)
+  require 'consort/active_record/mongo_mapper'
+  require 'consort/mongo_mapper/active_record' 
+end
+
+#if defined?(ActiveRecord::Base) && defined?()
